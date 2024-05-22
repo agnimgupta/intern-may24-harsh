399b8f293dc6f1cbaebe803632a439de6a1e52b1
Harsh Kain <72141797+harsh-kain@users.noreply.github.com>
2024-05-22 12:35:17 +0530

    adding starter code

---
+++ consumerSection/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-slate-600 text-5xl'>kain</h1>
+    </>
+  )
+}
+
+export default App
