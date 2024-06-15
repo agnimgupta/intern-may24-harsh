66299ae5d0768098f32d7abc04945f280414161d
Harsh Kain <72141797+harsh-kain@users.noreply.github.com>
2024-06-15 07:18:42 +0000

    fixing chat app

--- consumerSection/src/components/ChatList.jsx
+++ consumerSection/src/components/ChatList.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 const ChatList = ({ contacts, onSelect }) => {
 
   return (
-    <div className="w-1/3 border-r-2 border-gray-100 p-4">
-      <input
-        type="text"
-        placeholder="Search here"
-        className="w-full p-2 mb-4 border border-gray-300 rounded"
-      />
+    <div className="w-[50%] border-r-2 border-gray-100 p-4 justify-end items-center ">
+
+      <div className="flex justify-center mb-4 my-4 bg-gray-200 px-4 border rounded-lg  w-full m:w-[100%] xl:w-[100%] ">
+        <img src="/Search.svg" alt="" className='w-[30px] h-[auto] object-contain' />
+        <input
+          type="text"
+          placeholder="Search"
+          className="py-3 px-4 outline-none  w-full bg-transparent"
+        />
+      </div>
 
       <ul>
         {contacts.map((contact, index) => (
@@ -16,21 +20,26 @@ const ChatList = ({ contacts, onSelect }) => {
             key={index}
             className="flex items-center mb-4 cursor-pointer py-3"
             onClick={() => onSelect(contact)}
-            
+
           >
-            {/* {console.log(contact)} */}
             <img
               src={contact.image}
               alt={contact.name}
-              className="h-12 w-12 rounded-full mr-4"
+              className="h-9 w-9 rounded-full mr-4"
             />
-            <div className="hidden lg:block flex-1">
-              <div className="flex justify-between">
-                <h4 className="font-semibold">{contact.name}</h4>
-                <span className="text-sm text-gray-500">{contact.time}</span>
+            <div className="flex-1 flex justify-between items-start ">
+
+              <div className="flex justify-start items-start flex-col">
+                <h4 className="font-semibold text-sm">{contact.name}</h4>
+                <p className="text-sm text-gray-500 truncate">{contact.message}</p>
               </div>
-              <p className="text-sm text-gray-500 truncate">{contact.message}</p>
-            <span className=" bg-green-100 text-green-800 text-xs font-semibold px-2.5 py-0.5 rounded">{contact.newMessages}</span>
+
+              <div className="flex justify-end items-end flex-col ">
+                <span className="text-xs text-gray-500">{contact.time}</span>
+                <span className=" bg-[--primary] text-white text-xs font-semibold p-1 rounded-full w-5 h-5 text-center">{contact.newMessages}</span>
+              </div>
+
+
             </div>
           </li>
         ))}
